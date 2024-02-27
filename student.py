1f2a6a5c269072cabaa806258d9d9b0c6245c4b6
GRaF <161387217+rafscarlet@users.noreply.github.com>
2024-02-27 18:53:04 +0200

    Update student.py
    I added the Competition class and run it with an example of the CapySEEMUS cometition on an extra main

--- student.py
+++ student.py
@@ -1,7 +1,6 @@
 
 import random
 
-
 class Student:
     def __init__(self, name, age, subjects, grades):
         self.name = name
@@ -26,6 +25,7 @@ def randstudgen(n):
         stud.append(s)
         times.append(random.randint(0,10))
     return stud, times
+
 class School:
     def __init__(self,students:list[Student]):
         self.students=students
@@ -36,9 +36,11 @@ class School:
             avlist.append(i.avg)
         print(f'Maximum Average Score: {max(avlist)}')
         return avlist
+    
     def schoolinfo(self):
         for i in self.students:
             i.studinfo()
+
     def fail(self):
         failist=[]
         print(f'Failed:')
@@ -49,24 +51,92 @@ class School:
         if len(failist)==0:
             print("No Students Failed :)")
         return failist
+    
+    def best(self,subject):
+      
+        chosen=[]
+        while len(chosen)<2:
+            i=random.randint(0,len(self.students)-1)
+            p=self.students[i]
+            if p.grades[subject]>80:
+                chosen.append(p)
+        return chosen
+        
     def retrain(self, studytimes):
         for i,j in zip(self.students,studytimes):
             i.retrain(j)
         print('------------------------------Retraining Complete-------------------------------')
 
 
+def schoolgen(n,m): 
+    schoolist=[]
+    for i in range(n):
+        stud2,times2=randstudgen(m)
+        k=School(stud2)
+        schoolist.append(k)
+    return schoolist, times2
 
-stud1, times1=randstudgen(100)
-GelEvosmou=School(stud1)
-# GelEvosmou.schoolinfo()
-GelEvosmou.big()  
-failist=GelEvosmou.fail()
+class Competition:
+    def __init__(self,schools:list[School]):
+        self.schools=schools
 
-SummerSchool=School(failist)
-SummerSchool.schoolinfo()
-SummerSchool.retrain(times1)
-SummerSchool.schoolinfo()
-SummerSchool.big()
+    def runcompetition(self, subject, subcomp:list[Student]):
+        points=[]
+ 
+        for i in subcomp: 
+
+            points.append(random.random()*i.grades[subject])
+        subwinner=subcomp[points.index(max(points))]
+        winnerpoints=max(points)
+        return subwinner, winnerpoints
+        
+    def runall(self,comp):
+        winners=[]
+        points=[]
+        for i in range(3):
+            z,point=self.runcompetition(i,comp[i])
+            winners.append(z)
+            points.append(point)
+        print(f"The big winners are: ")
+        for  j,p in zip(winners,points):
+            j.studinfo()
+            print(f'Score: {p}')
+        return winners
+        
+        
+
+def main1():
+    stud1, times1=randstudgen(100)
+    GelEvosmou=School(stud1)
+    # GelEvosmou.schoolinfo()
+    GelEvosmou.big()  
+    failist=GelEvosmou.fail()
+
+    SummerSchool=School(failist)
+    SummerSchool.schoolinfo()
+    SummerSchool.retrain(times1)
+    SummerSchool.schoolinfo()
+    SummerSchool.big()
+
+def main2():
+    skg_schools,times_s=schoolgen(10,100)
+    subcomp1=[]
+    subcomp2=[]
+    subcomp3=[]
+    for i in skg_schools:
+        subcomp1.append(i.best(0)[0])
+        subcomp1.append(i.best(0)[1])
+
+        subcomp2.append(i.best(1)[0])
+        subcomp2.append(i.best(1)[1])
+
+        subcomp3.append(i.best(2)[0])
+        subcomp3.append(i.best(2)[1])
 
 
+    comp=[subcomp1,subcomp2,subcomp3]
+    capySEEMUS=Competition(skg_schools)
+    capySEEMUS.runall(comp)
 
+
+main2()
